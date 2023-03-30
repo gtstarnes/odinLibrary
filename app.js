32308f8955781528342d95a58235b6909b49ff65
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-03-30 13:00:21 -0400

    added read and remove functions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const addNew = document.getElementById("popUp");
 const container = document.getElementById("container");
 const addBook = document.getElementById('addBook');
 const cancel = document.getElementById('cancel');
-const book = document.getElementById('book1');
+const books = document.getElementsByClassName('book');
 
 function makeVisible() {
     container.style.visibility = "visible";
@@ -12,11 +12,20 @@ function makeHidden() {
     container.style.visibility = "hidden";
 }
 
-function markRead() {
-    book.style.backgroundColor = "SandyBrown";
+function initial(list) {
+   for (let i = 0; i < list.length; i++) {
+        list[i].addEventListener('dblclick', function () {
+            this.style.backgroundColor = "SandyBrown"; 
+        });
+        list[i].addEventListener('drag', function () {
+            this.remove();
+        })
+    }
 }
 
+
+initial(books);
 addNew.addEventListener('click', makeVisible);
 addBook.addEventListener('click', makeHidden);
 cancel.addEventListener('click', makeHidden);
-book.addEventListener('dblclick', markRead);
+
