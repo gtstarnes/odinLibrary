b764ed511e822260d062449a896cd464d265e13c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2023-03-30 16:46:24 -0400

    updated html and fixed library repeat issue

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const addNew = document.getElementById("popUp");
 const container = document.getElementById("container");
 const addBook = document.getElementById('addBook');
-//const cancel = document.getElementById('cancel');
+const cancel = document.getElementById('cancel');
 const bookshelf = document.getElementById('bookshelf')
-const library = [];
+
 
 // STYLE FUNCTIONS
 function makeVisible() {
@@ -14,7 +14,7 @@ function makeHidden() {
     container.style.visibility = "hidden";
 }
 
-function initial(list) {
+function listFunctions(list) {
    for (let i = 0; i < list.length; i++) {
         list[i].addEventListener('dblclick', function () {
             this.style.backgroundColor = 'SandyBrown'; 
@@ -36,57 +36,66 @@ function Book(title, author, genre, year) {
 
 // GETS FORM/BOOK INFO
 function makeNew() {
+    const library = [];
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let genre = document.getElementById('genre').value;
     let year = document.getElementById('year').value;
-    library.push( new Book(title,author,genre,year));
-    console.log(library);
-    return library;
+    
+    library.push(new Book(title,author,genre,year));
+    addBookToLibrary(library);
 }
-/*
-function addBookToLibrary() {
-    makeNew();
-    console.log(library);
 
+
+function addBookToLibrary(library) {
     for (let i = 0; i < library.length; i++) {
+        //creates divs
         let bookDiv = document.createElement('div');
         bookDiv.classList.add("book");
-        bookshelf.appendChild(bookDiv);
-
         let basicInfo = document.createElement('div');
         basicInfo.classList.add("basicBookInfo");
-        bookDiv.appendChild(basicInfo);
-
-        let bookTitle = document.createElement('p');
-        bookTitle.classList.add("title");
-        bookTitle.appendChild(library[i].title);
-        basicInfo.appendChild(bookTitle);
-
-        let bookAuthor = document.createElement('p');
-        bookAuthor.classList.add("author");
-        bookAuthor.appendChild(library[i].author);
-        basicInfo.appendChild(bookAuthor);
-
         let  extraInfo = document.createElement('div');
         extraInfo.classList.add("extraBookInfo");
-        bookDiv.appendChild(extraInfo);
-
+     
+        //creats p, adds class and text for each category
+        let bookTitle = document.createElement('p');
+        bookTitle.classList.add("title");
+        let content = document.createTextNode(library[i].title);
+        bookTitle.appendChild(content);
+        
+        let bookAuthor = document.createElement('p');
+        bookAuthor.classList.add("author");
+        content = document.createTextNode(library[i].author);
+        bookAuthor.appendChild(content);
+               
         let  bookGenre = document.createElement('p');
         bookGenre.classList.add("genre");
-        bookGenre.appendChild(library[i].genre);
+        content = document.createTextNode(library[i].genre);
+        bookGenre.appendChild(content);
+        
+
+        let  bookYear = document.createElement('p');
+        bookYear.classList.add("date");
+        content = document.createTextNode(library[i].year);
+        bookYear.appendChild(content);
+
+
+        basicInfo.appendChild(bookTitle);
+        basicInfo.appendChild(bookAuthor);
+        bookDiv.appendChild(basicInfo);
+
         extraInfo.appendChild(bookGenre);
+        extraInfo.appendChild(bookYear);
+        bookDiv.appendChild(extraInfo);
 
-        let  bookDate = document.createElement('p');
-        bookDate.classList.add("date");
-        bookDate.appendChild(library[i].date);
-        extraInfo.appendChild(bookDate);
+        bookshelf.appendChild(bookDiv);
     }
-    initial(document.getElementsByClassName('book'))
-}*/
+    listFunctions(document.getElementsByClassName('book'));
+}
 
-initial(document.getElementsByClassName('book'));
+listFunctions(document.getElementsByClassName('book'));
 addNew.addEventListener('click', makeVisible);
 addBook.addEventListener('click', makeNew);
-//cancel.addEventListener('click', makeHidden);
+addBook.addEventListener('click', makeHidden);
+cancel.addEventListener('click', makeHidden);
 
